1520de1fe1b3c28c5c1fd47494f1f9693e6a4b50
salimhass <salim.hassouneh@gmail.com>
2022-11-03 00:48:29 +0200

    adding out of stock

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,45 +1,72 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 import "./Products.css";
-import {Link} from "react-router-dom";
-import {connect} from 'react-redux';
-import basketWhite from "../../img/basketWhite.svg"
-
-
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import basketWhite from "../../img/basketWhite.svg";
 
 export class Products extends Component {
-
-    render() {
-        const currentCurrency = this.props.selectedCurrency
-        let price = this.props.product.prices[0]
-        for (const p of this.props.product.prices) {
-            if (p.currency.label === currentCurrency.label) {
-                price = p.amount
-                break;
-            }
-        }
-
-        return (
-
-            <Link className='links' to={`/product/${this.props.product.id}`}>
-                <div className='container'>
-
-                    <img className='prod--img' src={this.props.product.gallery[0]} alt="product img"/>
-                    <div className='prod--name'>{this.props.product.name}</div>
-                    <div className='prod--price'>{currentCurrency.symbol}{price}</div>
-                    <div className='basket--img--pro--circ'><img src={basketWhite} alt='basket--img--pro' className=''></img></div>
-
-                </div>
-            </Link>
-        )
+  render() {
+    const currentCurrency = this.props.selectedCurrency;
+    let price = this.props.product.prices[0];
+    for (const p of this.props.product.prices) {
+      if (p.currency.label === currentCurrency.label) {
+        price = p.amount;
+        break;
+      }
     }
+
+    return (
+      <>
+        {this.props.product.inStock ? (
+          <Link className="links" to={`/product/${this.props.product.id}`}>
+            <div className="container">
+              <img
+                className="prod--img"
+                src={this.props.product.gallery[0]}
+                alt="product img"
+              />
+              <div className="prod--name">{this.props.product.name}</div>
+              <div className="prod--price">
+                {currentCurrency.symbol}
+                {price}
+              </div>
+              <div className="basket--img--pro--circ">
+                <img
+                  src={basketWhite}
+                  alt="basket--img--pro"
+                  className=""
+                ></img>
+              </div>
+            </div>
+          </Link>
+        ) : (
+          <>
+            <div className="container--out"> 
+            <div className="text--outof">OUT OF STOCK</div>
+              <img
+                className="prod--img"
+                src={this.props.product.gallery[0]}
+                alt="product img"
+              />
+              <div className="prod--name">{this.props.product.name}</div>
+              <div className="prod--price">
+                {currentCurrency.symbol}
+                {price}
+              </div>
+              
+            </div>
+          </>
+        )}
+      </>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const selectedCurrency = state.currency.currency;
-    return {
-        selectedCurrency
-    };
+  const selectedCurrency = state.currency.currency;
+  return {
+    selectedCurrency,
+  };
 }
 
-
-export default connect(mapStateToProps)(Products)
+export default connect(mapStateToProps)(Products);
